0a6dfcd8ab170818957c0ed96defcfa047843553
ansenyi <15140265630@163.com>
2020-06-27 23:10:14 +0800

    安装vant

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,34 +1,43 @@
 <template>
   <div class="footer">
     <ul>
-      <li v-for='(item,index) of items' :class='[ item.cls , {on:index === idx} ]' @click="$router.push(item.push)">
-        {{item.name}}
-      </li>
+      <li
+        v-for="(item,index) of items"
+        :class="[ item.cls , {on:index === idx} ]"
+        @click="$router.push(item.push)"
+        :key="index"
+      >{{item.name}}</li>
     </ul>
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        items: [{
+export default {
+  data() {
+    return {
+      items: [
+        {
           cls: "loan",
           name: "借款",
           push: "/loan"
         },
-          {
-            cls: "credit",
-            name: "信用",
-            push: "/credit"
-          },
-          {
-            cls: "user",
-            name: "个人中心",
-            push: "/user"
-          }
-        ]
-      }
-    },
-    props: ['idx']
-  }
+        {
+          cls: "credit",
+          name: "信用",
+          push: "/credit"
+        },
+        {
+          cls: "user",
+          name: "个人中心",
+          push: "/user"
+        },
+        {
+          cls: "vantPractice",
+          name: "vant练习",
+          push: "/vantPractice"
+        }
+      ]
+    };
+  },
+  props: ["idx"]
+};
 </script>
---
+++ src/view/vantPractice/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    vant练习
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+<script>
+//引入页面数据
+
+export default {
+  data() {
+    return {
+      data: {},
+      items: []
+    };
+  },
+  methods: {
+    loginOut() {}
+  }
+};
+</script>
